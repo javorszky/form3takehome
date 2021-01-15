718e2359cd69ca419c89e9fd6d125847fa2c600c
Gabor Javorszky <gabor@javorszky.co.uk>
2021-01-15 09:24:55 +0000

    added structs for payload data

--- pkg/client/resource.go
+++ pkg/client/resource.go
@@ -1,5 +1,7 @@
 package client
 
+import "time"
+
 type Resource struct {
 	Country                 string    `json:"country"`
 	BaseCurrency            string    `json:"base_currency"`
@@ -17,4 +19,26 @@ type Resource struct {
 	SecondaryIdentification string    `json:"secondary_identification"`
 	Switched                bool      `json:"switched"`
 	Status                  string    `json:"status"`
+}
+
+type Data struct {
+	ID             string    `json:"id"`
+	OrganisationID string    `json:"organisation_id"`
+	Type           string    `json:"type"`
+	Version        int       `json:"version"`
+	CreatedOn      time.Time `json:"created_on"`
+	ModifiedOn     time.Time `json:"modified_on"`
+	Attributes     Resource  `json:"attributes"`
+}
+
+type Links struct {
+	Self  string `json:"self"`
+	First string `json:"first,omitempty"`
+	Next  string `json:"next,omitempty"`
+	Last  string `json:"last,omitempty"`
+}
+
+type Payload struct {
+	Data  Data  `json:"data"`
+	Links Links `json:"links"`
 }
