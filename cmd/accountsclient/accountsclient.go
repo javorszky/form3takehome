75eb970ecc8f426b16e2eb7d115c2994ab3c7042
Gabor Javorszky <gabor@javorszky.co.uk>
2021-01-19 23:20:24 +0000

    add example implementation

--- cmd/accountsclient/accountsclient.go
+++ cmd/accountsclient/accountsclient.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/javorszky/form3takehome/pkg/client"
+	"github.com/javorszky/form3takehome/pkg/config"
+)
+
+const timeOutExampleMs = 500
+
+func main() {
+	// this is an example implementation
+	cfg, err := config.Get()
+	if err != nil {
+		log.Fatalf("failed to get config: %s", err)
+	}
+
+	httpClient := http.Client{
+		Timeout: timeOutExampleMs * time.Millisecond,
+	}
+
+	gmtLoc, err := time.LoadLocation("GMT")
+	if err != nil {
+		log.Fatalf("failed to load gmt timezone: %s", err)
+	}
+
+	c := client.New(cfg, httpClient, gmtLoc)
+
+	p, err := c.Create(client.Resource{
+		Country:    "GB",
+		BankIDCode: "GBDSC",
+		BIC:        "BARCGB22XXX",
+		BankID:     "123456",
+	})
+
+	fmt.Printf("inserted a resource and this is what the service said with no error: %s: %#v", err, p)
 }
